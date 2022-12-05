2d7f85e4d7241ced9f368bf245dd7b9531c62a7e
Kefah Issa <kefah@imx.sh>
2022-12-05 04:53:41 +0300

    update

---
+++ src/routes/me.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Accordion } from "stwui";
+
+  let open = "";
+
+  function handleClick(item: string) {
+    if (open === item) {
+      open = "";
+    } else {
+      open = item;
+    }
+  }
+</script>
+
+<Accordion>
+  <Accordion.Item open={open === "item1"}>
+    <Accordion.Item.Title slot="title" on:click={() => handleClick("item1")}>
+      Item 1
+    </Accordion.Item.Title>
+    <Accordion.Item.Content slot="content">
+      accordion_body
+    </Accordion.Item.Content>
+  </Accordion.Item>
+  <Accordion.Item open={open === "item2"}>
+    <Accordion.Item.Title slot="title" on:click={() => handleClick("item2")}>
+      Item 2
+    </Accordion.Item.Title>
+    <Accordion.Item.Content slot="content">
+      accordion_body
+    </Accordion.Item.Content>
+  </Accordion.Item>
+  <Accordion.Item open={open === "item3"}>
+    <Accordion.Item.Title slot="title" on:click={() => handleClick("item3")}>
+      Item 3
+    </Accordion.Item.Title>
+    <Accordion.Item.Content slot="content">
+      accordion_body
+    </Accordion.Item.Content>
+  </Accordion.Item>
+</Accordion>
